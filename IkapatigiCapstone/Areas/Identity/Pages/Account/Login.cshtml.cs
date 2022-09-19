9c8c6f0b0472a6e0811286bb40c94216e5630a40
GabrielleSingh <59948459+GabrielleSingh@users.noreply.github.com>
2022-09-19 23:03:16 +0800

    Login & Register are invalid

--- IkapatigiCapstone/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ IkapatigiCapstone/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -109,7 +109,7 @@ namespace IkapatigiCapstone.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
 
             if (ModelState.IsValid)
-            {
+            { 
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
                 var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
