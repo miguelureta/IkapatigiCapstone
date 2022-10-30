7a67201542f0400b24275e6999d06c6fce93fdd9
GabrielleSingh <59948459+GabrielleSingh@users.noreply.github.com>
2022-10-30 19:33:33 +0800

    Made  a separate project just to see if I could get Forums working in a separate one bc I need "Authorization" to be configured as Inidividual User Accounts for the app to work, so I'm just doing it from scratch. Will figure out how to move it into the project later if this works.

---
+++ IkapatigiForums/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IkapatigiForums.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
